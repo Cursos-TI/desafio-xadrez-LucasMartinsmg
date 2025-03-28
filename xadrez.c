30f4e55af72d5fb6ee867715aa2f626f682cafc3
LucasMartinsmg <lucasamptrader@gmail.com>
2025-03-28 12:49:57 +0000

    movimento cavalo finalizado

--- xadrez.c
+++ xadrez.c
@@ -18,7 +18,7 @@ int main() {
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    int bispo = 1, torre = 1, rainha = 1 ; //VARIAVEIS PARA MOVIMENTAÇÃO DO JOGO
+    int bispo = 1, torre = 1, rainha = 1, cavalo = 10; //VARIAVEIS PARA MOVIMENTAÇÃO DO JOGO
     printf("#### MOVIMENTAÇÃO JOGO XADREZ ####\n Movimentação Bispo: \n");
 
     while (bispo <= 5) //Bispo: 5 casas na diagonal superior direita
@@ -30,16 +30,31 @@ int main() {
 
     do // Torre: 5 casas para a direita
     {
-       printf("Movimentação torre:\n");
+       printf("Movimentação Torre:\n");
        printf("Movimento: %d casas para a direita\n", torre);
         torre ++ ;
     } while (torre <= 5) ;
-    printf("MOVIMENTO TORRE FINALIZADA\n");
+    printf("MOVIMENTO TORRE FINALIZADA\n\n");
 
     for (int rainha = 1 ; rainha <= 8 ; rainha++ ) { //Rainha: 8 casas para a esquerda
         printf("Movimento: %d casas para a esquerda\n", rainha);
     } 
     printf("MOVIMENTO RAINHA FINALIZADO\n\n");
+
+    //MOVIMENTO CAVALO: O cavalo deve se movimentar para baixo e para a esquerda, usando pelo menos duas estruturas de repetição: uma deve ser for e a outra pode ser while ou do-while.
+
+    printf("Movimentação Cavalo:\n");
+    while(cavalo -- )
+    {
+        for( int i = 0; i < 2; i++)
+        {
+            printf("baixo\n"); // Imprime "Baixo" duas vezes
+        }
+     printf("Esquerda\n"); //Imprime "Esquerda" uma vez   
+    }
+    printf("MOVIMENTO CAVALO FINALIZADO\n\n");
+
+
     
     
 
